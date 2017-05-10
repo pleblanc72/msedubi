ff7d90e8b095dbad252f5183ea2941a1363239cd
pleblanc72 <patrick@sqllunch.com>
2017-05-10 18:19:37 +0000

    remove

--- HigherED BI Solution/On Premises/Data Warehouse/HigherED/HigherED.DataWarehouse/dbo/Tables/DimAcademicProgram.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[DimAcademicProgram] (
-    [AcademicProgramSK] INT          IDENTITY (1, 1) NOT NULL,
-    [AcademicProgramAK] VARCHAR (10) NOT NULL,
-    [AcademicProgram]   VARCHAR (50) NOT NULL,
-    [AcademicPlanAK]    VARCHAR (10) NOT NULL,
-    [AcademicPlan]      VARCHAR (50) NOT NULL,
-    CONSTRAINT [PK_AcademicProgram_AcademicProgramSK] PRIMARY KEY CLUSTERED ([AcademicProgramSK] ASC)
-);
-
