36c9bf3b8680159e7cf0a9aaf471e09fc1f9deeb
pleblanc72 <patrick@sqllunch.com>
2017-05-10 18:26:47 +0000

    commit

---
+++ HigherED BI Solution/On Premises/Data Warehouse/HigherED/HigherED.DataWarehouse/dbo/Tables/DimClass.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[DimClass] (
+    [ClassSK]           INT          IDENTITY (1, 1) NOT NULL,
+    [SubjectAK]         VARCHAR (10) NOT NULL,
+    [CatalogAK]         VARCHAR (10) NOT NULL,
+    [ClassSectionAK]    VARCHAR (5)  NOT NULL,
+    [Title]             VARCHAR (50) NULL,
+    [CreditHours]       INT          NULL,
+    [InstructionModeAK] VARCHAR (35) NULL,
+    CONSTRAINT [PK_Class_ClassSK] PRIMARY KEY CLUSTERED ([ClassSK] ASC)
+);
+
