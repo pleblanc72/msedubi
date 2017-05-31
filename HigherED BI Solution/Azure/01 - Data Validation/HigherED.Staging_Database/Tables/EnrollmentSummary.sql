ff1e9c03d0cc81d565a81ba3229562c4689ecae7
Steve Pontello <spontello@outlook.com>
2017-05-31 15:00:24 -0700

    Added 01-Data Validation to Azure Fork

---
+++ HigherED BI Solution/Azure/01 - Data Validation/HigherED.Staging_Database/Tables/EnrollmentSummary.sql
@@ -0,0 +1,21 @@
+﻿
+CREATE TABLE [dbo].[EnrollmentSummary](
+	[EnrollmentSummaryID] [int] IDENTITY(1,1) NOT NULL,
+	[Term] [varchar](50) NULL,
+	[CRMID] [varchar](50) NULL,
+	[Residential_Commuter] [varchar](50) NULL,
+	[InState_OutState] [varchar](50) NULL,
+	[Term_Credit_Attempted] [varchar](50) NULL,
+	[Term_Credits_Earned] [varchar](50) NULL,
+	[Term_GPA] [varchar](50) NULL,
+	[Transfer_Credit] [varchar](50) NULL,
+	[Cum_Credits_Attempted] [varchar](50) NULL,
+	[Cum_Credits_Earned] [varchar](50) NULL,
+	[Cum_GPA] [varchar](50) NULL,
+	[Academic_Level] [varchar](50) NULL,
+	[Academic_Standing] [varchar](50) NULL,
+CONSTRAINT PK_EnrollmentSummary_EnrollmentSummaryID PRIMARY KEY CLUSTERED 
+(
+	[EnrollmentSummaryID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
---
+++ HigherED BI Solution/Azure/01 - Data Validation/HigherED.Staging_Database/Views/Admission.sql
@@ -0,0 +1,16 @@
+﻿CREATE VIEW dbo.Admission
+AS
+SELECT 
+	CAST(CRMID AS bigint) StudentAK,
+	--CAST([ADMIT_TERM] AS int) TermAK,
+	CAST(CONVERT( varchar(10),CAST(t.StartDate AS date), 112) AS int) AdmitDateAK,
+	CAST([ADMIT_TYPE] AS varchar(5)) AdmitTypeAK,
+	LEFT([APPL_SOURCE], 1) ApplicationSourceAK,
+	CAST([ACAD_PROG] AS varchar(10)) AcademicProgramAK,
+	CAST([ACAD_PLAN] AS varchar(10)) AcademicPlanAK,
+	CAST([CURRENT_PROGRAM] AS varchar(10)) CurrentAcademicProgramAK,
+	CAST([CURRENT_PLAN] AS varchar(10)) CurrentAcademicPlanAK
+FROM dbo.StudentProfile sp
+INNER JOIN dbo.Term t
+	ON sp.ADMIT_TERM = t.TermAK
+GO
